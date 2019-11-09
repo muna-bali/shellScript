dd5463ca6980321331c1cdd7dc33dd4e7b226614
muna-bali <munabali@live.com>
2019-11-09 18:36:14 +0200

    Add files via upload

---
+++ shell.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+##############
+#Test for scripting 
+#shell scripting
+##############
+ cd ~
+ mkdir test
+ cd test
+ git clone https://github.com/muna-bali/javatest.git
+ cd javatest/src
+ ls
+ javac /javatest/src/*.java
+ java BSTtest
